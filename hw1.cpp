70642c86a493c2451f834b6478e8062e242135e7
gg1140 <gordonlschan@gmail.com>
2019-04-17 02:16:05 -0700

    Cleaned Up & Refinement

--- hw1.cpp
+++ hw1.cpp
@@ -1,6 +1,3 @@
-// color img > b&w img
-// box filter that avg all image pixels (blurring)
-
 #include <opencv2/opencv.hpp>
 
 using namespace std;
@@ -24,65 +21,86 @@ Mat convert2grey(Mat color_img) {
     return bw_img;
 }
 
-Mat swap_color_channels(Mat og_img) {
-    Mat new_img(og_img.rows, og_img.cols, CV_8UC3);
-
-    for (int i = 0; i < og_img.rows; i++) {
-        for (int j = 0; j < og_img.cols; j++) {
-            Vec3b pixel = og_img.at<Vec3b>(i, j);
-            Vec3b new_pixel = {pixel[1], pixel[0], pixel[2]}; //Switching green with blue
-
-            //Vec3b new_pixel = {pixel[0], pixel[2], pixel[1]}; //Switching red with green
-
-            //Vec3b new_pixel = {pixel[2], pixel[1], pixel[0]}; //Switching blue with red
-            new_img.at<Vec3b>(i, j) = new_pixel;
+/*
+ * kernal h[n] is a M points signal
+ * for i = 0 to M - 1, sum(h[j] * x[i - j])
+ * Both Mat parameters must have the same dimension
+ * Both mat parameters must be single channel
+ */
+template <typename kT, typename sT> //kernal type, sample type
+sT convol_sum(Mat& kernal, Mat& samples) {
+    if (kernal.total() == samples.total()) {
+        float sum = 0;
+        for (int i = 0; i < kernal.rows; i++) {
+            for (int j = 0; j < kernal.cols; j++) {
+                float sample_pix = (float)samples.at<sT>(i, j);
+                float kernal_pix = (float)kernal.at<kT>(i, j);
+                sum += sample_pix * kernal_pix;
+            }
         }
+        return (sT)round(sum);
+    } else {
+        printf("Error: size of input Mats does not matches");
+        exit(-1);
     }
-    return new_img;
 }
 
-int reflect(Mat img, int x, int y) {
+/*
+ * Return a Point type containing row and col position
+ * obtained from reflecting a position outside of the img boundary
+ * with its closest img boundary row & col position
+ * Point(col, row), Point.x = col, Point.y = row
+ */
+Point reflect(int x, int y, int height, int width) {
     auto below_bound = [](int x) {return x < 0;};
     auto over_bound = [](int x, int upper) {return x > upper;};
-    auto reflect_below = [](int x) {return (x * -1) - 1;};
-    auto reflect_over = [](int x, int upper) {return (upper * 2 - 1) - x;};
+    auto reflect_index = [] (int x, int edge_val) {return (2 * edge_val) - 1 - x;};
+
     int new_x = x;
     int new_y = y;
+
     if (below_bound(x)) {
-        new_x = reflect_below(x);
-    } else if (over_bound(x, img.rows - 1)) {
-        new_x = reflect_over(x, img.rows);
+        new_x = reflect_index(x, 0);
+    } else if (over_bound(x, height - 1)) {
+        new_x = reflect_index(x, height);
     }
 
     if (below_bound(y)) {
-        new_y = reflect_below(y);
-    } else if (over_bound(y, img.cols - 1)) {
-        new_y = reflect_over(y, img.cols);
+        new_y = reflect_index(y, 0);
+    } else if (over_bound(y, width - 1)) {
+        new_y = reflect_index(y, width);
     }
-    return img.at<int>(new_x, new_y);
+
+    return Point(new_y, new_x);
 }
 
-Mat fill_kernal3(Mat img, int mid_x, int mid_y) {
+/*
+ * Point(col, row), Point.x = col, Point.y = row
+ */
+template <typename T>
+Mat gather_samples(Mat& img, Point pix_pos, Size kernal) {
     auto out_bound = [c=img.cols-1, r=img.rows-1](int x, int y) {
         return (x < 0 || x > r || y < 0 || y > c);
     };
-    int offset_x = mid_x - 1;
-    int offset_y = mid_y - 1;
-    Mat kernal(3, 3, CV_8UC1);
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            int lin_ind = i*3 + j;
-            int temp_x = offset_x + i;
-            int temp_y = offset_y + j;
-            if (out_bound(temp_x, temp_y)) {
-                kernal.at<uchar>(i, j) = reflect(img, temp_x, temp_y);
-                //kernal.at<uchar>(i, j) = 0; uncomment this for zero padding
+
+    Mat samples(kernal, img.type());
+    int x = pix_pos.y - kernal.height/2; //x,y are 1st element's position
+    int y = pix_pos.x - kernal.width/2; //of the kernal centered at pix_pos
+
+    for (int i = 0; i < samples.rows; i++) {
+        for (int j = 0; j < samples.cols; j++) {
+            int ofset_x = x + i;
+            int ofset_y = y + j;
+            if (out_bound(ofset_x, ofset_y)) {
+                samples.at<T>(i, j) =
+                    img.at<T>(reflect(ofset_x, ofset_y, img.rows, img.cols));
+                //kernal.at<uchar>(i, j) = 0; //uncomment this for zero padding
             } else {
-                kernal.at<uchar>(i, j) = img.at<uchar>(temp_x, temp_y);
+                samples.at<T>(i, j) = img.at<T>(ofset_x, ofset_y);
             }
         }
     }
-    return kernal;
+    return samples;
 }
 
 /*
@@ -92,24 +110,34 @@ Mat fill_kernal3(Mat img, int mid_x, int mid_y) {
  * 3. the kernal/filter dimension is 3x3
  */
 Mat BoxFilterGray3(Mat og_bw_img) {
-    /*auto edge = [c=og_bw_img.cols-1, r=og_bw_img.rows-1](int x, int y) {
+    auto edge = [c=og_bw_img.cols-1, r=og_bw_img.rows-1](int x, int y) {
         return (x == 0 || x == c || y == 0 || y == r);
-    };*/
+    };
+
+    float filter_val[9] = {1/9.0f, 1/9.0f, 1/9.0f,
+                            1/9.0f, 1/9.0f, 1/9.0f,
+                            1/9.0f, 1/9.0f, 1/9.0f};
+    Mat filter = Mat_<float>(3, 3, filter_val);
     Mat new_img(og_bw_img.rows, og_bw_img.cols, CV_8UC1);
-    //Mat1i kernal(kernal_dim, kernal_dim, CV_8UC1);
-    for (int i = 0; i < og_bw_img.rows; i++) {
-        for (int j = 0; j < og_bw_img.cols; j++) {
-            Mat kernal = fill_kernal3(og_bw_img, i, j);
-            int sum = 0;
-            for (int s = 0; s < 3; s++) {
-                for (int t = 0; t < 3; t++) {
-                    sum += kernal.at<uchar>(s, t);
-                }
-            }
-            new_img.at<uchar>(i, j) = uchar(sum / 9.0);
+
+    for (int i = 0; i < new_img.rows; i++) {
+        for (int j = 0; j < new_img.cols; j++) {
+            Mat samples =
+                gather_samples<uchar>(og_bw_img, Point(j, i), filter.size());
+            new_img.at<uchar>(i, j) = convol_sum<float, uchar>(filter, samples);
         }
     }
     return new_img;
+}
+
+template <typename T>
+void printMat(Mat& img) {
+    for (int i = 0; i < img.rows; i++) {
+        for (int j = 0; j < img.rows; j++) {
+            cout << "|" << img.at<T>(i, j);
+        }
+        cout << endl;
+    }
 }
 
 int main(int argc, char** argv) {
@@ -118,15 +146,14 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    Mat og_img;
-    og_img = imread(argv[1], IMREAD_COLOR);//IMREAD_GRAYSCALE);
+    Mat og_img = imread(argv[1], IMREAD_GRAYSCALE);
 
     if (!og_img.data) {
         printf("No image data \n");
         return -1;
     }
 
-    Mat new_img = swap_color_channels(og_img);
+    Mat new_img = BoxFilterGray3(og_img);//swap_color_channels(og_img);
 
     if (argc = 3) {
         imwrite(argv[2], new_img);
